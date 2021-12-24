802f905e8d5be0351adcc206e6f6103b8e741a54
Michael <michaelbeverborg@hotmail.com>
2021-12-24 15:30:28 +0100

    #5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 // Function starts with a capital letter
 // JSX function
@@ -6,8 +8,10 @@ function App() {
   return (
     // Uses className because class is a reserved keyword of JS
     <div className="App">
+      {/* Import the Navbar at this location. Self-closing*/}
+      <Navbar />
       <div className="content">
-        <h1>App component</h1>
+        <Home />
       </div>
     </div>
   );
