7b195cb077aeac820ce755f5dcf3599268aada21
Michael <michaelbeverborg@hotmail.com>
2021-12-24 14:53:24 +0100

    Start

--- src/App.js
+++ src/App.js
@@ -3,28 +3,11 @@ import "./App.css";
 // Function starts with a capital letter
 // JSX function
 function App() {
-  // Variables are converted to strings except booleans and objects
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  //const person = { name: "Yoshi", age: 11 };
-  const link = "http://www.google.com";
-
   return (
     // Uses className because class is a reserved keyword of JS
     <div className="App">
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times!</p>
-
-        {/* Comment needs {} around it
-        /*<p>{person}</p>*/}
-
-        <p>{10}</p>
-        <p>{"Hello..."}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Website</a>
+        <h1>App component</h1>
       </div>
     </div>
   );
