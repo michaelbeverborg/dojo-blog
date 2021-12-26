655a528e253c9df1ffa92fc3ac732591787ea2b9
Michael <michaelbeverborg@hotmail.com>
2021-12-26 20:57:00 +0100

    #8

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,25 @@
-const Home = () => {
-  const handleClick = e => {
-    console.log("Hello there!", e);
-  };
+import { useState } from "react";
 
-  const handleClickToo = (name, e) => {
-    console.log("Hello " + name, e.target);
+const Home = () => {
+  // Instead of regular variable we need to use useState to change
+  // the variable reactively.
+  // const [variable name, function that changes the value] = useState(Starting value);
+  const [name, setName] = useState("Mario");
+  const [age, setAge] = useState(20);
+
+  const handleClick = () => {
+    setName("Luigi");
+    setAge(30);
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old.
+      </p>
       {/* No () behdind the funtion name else is runs immediately or use an arrow function */}
       <button onClick={handleClick}>Click here</button>
-      <button
-        onClick={e => {
-          handleClickToo("Yoshi", e);
-        }}
-      >
-        Click here too
-      </button>
     </div>
   );
 };
