fa7a1fe3feb9b09a1f239583538ab4f1515006cc
Michael <michaelbeverborg@hotmail.com>
2021-12-26 21:00:43 +0100

    #8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 
+// Function starts with a capital letter
+// JSX function
 function App() {
   return (
+    // Uses className because class is a reserved keyword of JS
     <div className="App">
+      {/* Import the Navbar at this location. Self-closing*/}
       <Navbar />
       <div className="content">
         <Home />
@@ -12,4 +16,5 @@ function App() {
   );
 }
 
+// Export to make it available for other files
 export default App;
