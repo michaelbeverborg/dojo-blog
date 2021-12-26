9fac46113dcbf5135976c76ea510ebf2eae69b8f
Michael <michaelbeverborg@hotmail.com>
2021-12-26 14:08:57 +0100

    #7

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
-// Function starts with a capital letter
-// JSX function
 function App() {
-  // Variables are converted to strings except booleans and objects
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  //const person = { name: "Yoshi", age: 11 };
-  const link = "http://www.google.com";
-
   return (
-    // Uses className because class is a reserved keyword of JS
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times!</p>
-
-        {/* Comment needs {} around it
-        /*<p>{person}</p>*/}
-
-        <p>{10}</p>
-        <p>{"Hello..."}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Website</a>
+        <Home />
       </div>
     </div>
   );
 }
 
-// Export to make it available for other files
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,24 @@
 const Home = () => {
+  const handleClick = e => {
+    console.log("Hello there!", e);
+  };
+
+  const handleClickToo = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      {/* No () behdind the funtion name else is runs immediately or use an arrow function */}
+      <button onClick={handleClick}>Click here</button>
+      <button
+        onClick={e => {
+          handleClickToo("Yoshi", e);
+        }}
+      >
+        Click here too
+      </button>
     </div>
   );
 };
