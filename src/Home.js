5042f645a19a493bb0dbf2a3646a04b6ae59f163
Michael <michaelbeverborg@hotmail.com>
2022-01-09 21:09:15 +0100

    #17

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   /* Argument 'id' is the blog.id that's been clicked on from BlogList.js */
   const handleDelete = id => {
@@ -25,16 +16,26 @@ const Home = () => {
 
   // useEffect runs at initialization of the component AND everytime the date (useState) changes
   useEffect(() => {
-    console.log("useEffect ran");
-    console.log(blogs);
+    fetch("http://localhost:8000/blogs")
+      .then(res => {
+        return res.json();
+      })
+      // below .then happens after the return from .then above happens
+      .then(data => {
+        setBlogs(data);
+      });
     // An empty array dependency makes the useEffect only run once at initialization
     // With name inside, it will run also when name changes
-  }, [name]);
+  }, []);
 
   return (
     <div className="home">
       {/* Blogs in BlogList.js is the blogs from here, send as argument */}
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {/* If blogs is true than what comes on the right && happens (conditional templating) */}
+      {/* blogs is null from the start so it doesn't continu, after the fetch is complete: blogs is not null */}
+      {blogs && (
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      )}
       <button onClick={() => setName("luigi")}>Change name</button>
       <p>{name}</p>
 
