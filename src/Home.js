7928207851a729380f01b45f543e60993e94d6bd
Michael <michaelbeverborg@hotmail.com>
2022-01-10 12:38:36 +0100

    #18

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setPending] = useState(true);
 
   /* Argument 'id' is the blog.id that's been clicked on from BlogList.js */
   const handleDelete = id => {
@@ -18,11 +19,14 @@ const Home = () => {
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then(res => {
+        // response received and return it as json
         return res.json();
       })
       // below .then happens after the return from .then above happens
+      // put data in a function that puts it in setBlogs
       .then(data => {
         setBlogs(data);
+        setPending(false);
       });
     // An empty array dependency makes the useEffect only run once at initialization
     // With name inside, it will run also when name changes
@@ -30,9 +34,11 @@ const Home = () => {
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
+
       {/* Blogs in BlogList.js is the blogs from here, send as argument */}
       {/* If blogs is true than what comes on the right && happens (conditional templating) */}
-      {/* blogs is null from the start so it doesn't continu, after the fetch is complete: blogs is not null */}
+      {/* blogs is null from the start so it doesn't continue, after the fetch is complete: blogs is not null */}
       {blogs && (
         <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
       )}
