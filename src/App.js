cdd39c58e5044437e9d5f67a59b20db08eacf250
Michael <michaelbeverborg@hotmail.com>
2022-01-16 11:00:25 +0100

    #21

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// The react router intercepts requests to the server, for example when another page is clicked.
+// React will then inject the page into the browser.
 
 // Function starts with a capital letter
 // JSX function
 function App() {
   return (
-    // Uses className because class is a reserved keyword of JS
-    <div className="App">
-      {/* Import the Navbar at this location. Self-closing*/}
-      <Navbar />
-      <div className="content">
-        <Home />
+    // Encapsule the whole website in the router
+    <Router>
+      {/**Uses className because class is a reserved keyword of JS */}
+      <div className="App">
+        {/* Import the Navbar at this location. Self-closing*/}
+        <Navbar />
+        <div className="content">
+          {/** The switch component makes sure only one route(page) is shown */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
