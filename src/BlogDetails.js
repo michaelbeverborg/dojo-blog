219b974d32a4ffc762b68d12a186020335666b84
Michael <michaelbeverborg@hotmail.com>
2022-01-22 11:42:52 +0100

    #26

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,28 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   // Get :id from app.js in the link
   const { id } = useParams();
+  // Import data named as blog. Reusing useFetch
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs/" + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id}</h2>
+      {/** Using the same stuff as in Home.js */}
+      {isPending && <div>Loading...</div>}
+      {error && <div>Error</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
